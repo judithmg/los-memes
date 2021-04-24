d7dc570c5ac0a765368065ea978ec6f5adf04f3e
judithmg <jmartigo33@gmail.com>
2021-04-24 13:28:06 +0200

    updated files

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,8 @@
 import React from "react";
-import "./App.css";
-import { DndProvider, useDrag } from "react-dnd";
+import "./App.scss";
+import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import Example from "./custom-drag-layer";
-
-const MemeType = {
-  MEME: "Meme",
-};
-
-const Text = () => {
-  const [{ isDragging }, drag] = useDrag(() => ({
-    type: MemeType.MEME,
-    collect: (monitor) => ({
-      isDragging: !!monitor.isDragging(),
-    }),
-  }));
-
-  return (
-    <span
-      ref={drag}
-      style={{
-        opacity: isDragging ? 0.5 : 1,
-        fontSize: 25,
-        fontWeight: "bold",
-        cursor: "move",
-      }}
-    >
-      MeMeS
-    </span>
-  );
-};
-
-const Meme = () => {
-  return (
-    <img
-      style={{ width: "600px" }}
-      src="https://pyxis.nymag.com/v1/imgs/7fa/2ed/2bda5e6dd39bf7f26a7283a584e796f3c1-24-disloyal-man-meme.rsquare.w700.jpg"
-      alt="meme"
-    />
-  );
-};
 
 const App = () => {
   return (
