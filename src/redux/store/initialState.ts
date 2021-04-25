e8e4fc770db6cb76e2f8b64956c6710425fc9b27
judithmg <jmartigo33@gmail.com>
2021-04-25 16:01:50 +0200

    configured redux store

---
+++ src/redux/store/initialState.ts
@@ -0,0 +1,14 @@
+const initialState = {
+  memes: [],
+  url: "https://i.imgflip.com/1ur9b0.jpg",
+  customize: {
+    color: "",
+    font: "",
+    boxes: {
+      0: { top: -400, left: 0 },
+      1: { top: -300, left: 500 },
+    },
+  },
+};
+
+export default initialState;
